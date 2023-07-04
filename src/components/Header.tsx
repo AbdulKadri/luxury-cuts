5bab5147b6a02ba29434a23243300989da21ae32
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-04 15:11:57 -0600

    Add mobile functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex items-center justify-between p-5 font-bold text-white bg-black border-b-2 border-b-primary h-[5rem] shadow-xl">
-      <div className="flex gap-2 items-center">
-        <img
-          src="src\assets\Luxury-Cuts-Logo.svg"
-          alt="Logo"
-          //   className="h-[2rem]"
-        />
-        <p className="text-2xl">Luxury Cuts</p>
+    <div className="flex items-center justify-between py-5 px-8 font-bold text-white bg-black border-b-2 border-b-primary h-[5rem] shadow-xl">
+      <div>
+        <a href="/" className="flex gap-2 items-center">
+          <img
+            src="src\assets\Luxury-Cuts-Logo.svg"
+            alt="Logo"
+            //   className="h-[2rem]"
+          />
+          <p className="text-2xl">Luxury Cuts</p>
+        </a>
       </div>
-      <ul className="flex gap-6 items-center">
+      <div className="md:hidden">
+        <button onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="2xl" />
+        </button>
+        {isOpen && (
+          <div className="absolute top-8 right-0 mt-12 bg-black w-full shadow-lg z-10">
+            <ul className="flex flex-col items-center gap-6 p-4 text-xl">
+              <li>Home</li>
+              <li>Services</li>
+              <li>Products</li>
+              <li>Contact</li>
+              <li>
+                <button className="bg-primary p-2 border border-white">
+                  Book Now
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <ul className="hidden md:flex gap-6 items-center">
         <li>Home</li>
         <li>Services</li>
         <li>Products</li>
