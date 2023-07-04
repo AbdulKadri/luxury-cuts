a5f2290dcf3915b5d34615d636097b54fcd9216b
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-04 15:09:45 -0600

    Add font-family and screens themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,17 @@ export default {
         'text-primary': '#000000',
         'text-secondary': '#FFFFFF',
       },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'Roboto': ['Roboto', 'sans-serif'],
+      },
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1200px',
+        '2xl': '1440px',
+      },
     },
   },
   plugins: [],
