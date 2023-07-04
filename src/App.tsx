2ccf43db351c391e0f05bec25a0b3eb15b8cdb9a
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-04 15:11:42 -0600

    Add layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Layout from "./Layout";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <Layout>
       <Header />
       <h1 className="text-3xl">Initial setup</h1>
-    </>
+    </Layout>
   );
 }
 
