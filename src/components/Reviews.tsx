ef5828a2f1603a160bb8e9f3101ee84324f63806
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-05 02:26:15 -0600

    Create review cards

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,12 @@
+import ReviewCard from "./ReviewCard";
+
+const Reviews = () => {
+  return (
+    <div className="flex justify-center items-center gap-6 bg-black">
+      <ReviewCard />
+      <ReviewCard />
+    </div>
+  );
+};
+
+export default Reviews;
