ea481ce6621736a1487245e505bdef5dee5b7c52
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-04 21:21:59 -0600

    Add services and products components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Layout from "./Layout";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Products from "./components/Products";
 
 function App() {
   return (
     <Layout>
       <Header />
       <Hero />
+      <Services />
+      <Products />
     </Layout>
   );
 }
