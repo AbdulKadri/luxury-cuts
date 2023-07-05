50c8659e613c405e081cf469ad8bf60c27b8872c
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-05 02:24:35 -0600

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-around bg-gradient-to-r from-white to-primary py-3 px-6">
+      <div className="flex items-center gap-2">
+        <img src="./Luxury-Cuts-Logo.svg" alt="Luxury Cuts Logo" />
+        <h2 className="font-bold text-2xl">
+          Luxury
+          <br />
+          Cuts
+        </h2>
+      </div>
+
+      <div>
+        <h4 className="font-semibold text-lg">Menu</h4>
+        <ul className="text-sm">
+          <li>Home</li>
+          <li>Services</li>
+          <li>Products</li>
+          <li>Contact</li>
+          <li>Book Now</li>
+        </ul>
+      </div>
+
+      <div>
+        <h4 className="font-semibold text-lg">Connect</h4>
+        <ul className="text-sm">
+          <li>780-111-1111</li>
+          <li>
+            10291 111st SW,
+            <br />
+            Edmonton Alberta, T11-1G1
+          </li>
+          <li>Test@gmail.com</li>
+        </ul>
+      </div>
+
+      <div>
+        <h4 className="font-semibold text-lg">Social's</h4>
+        <ul className="text-sm">
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+          <li>LinkedIn</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
