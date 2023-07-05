f260564315c31104c844cb39b590d843e586a1c0
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-05 02:24:18 -0600

    Fix styling and add extra text for desktop

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -8,7 +8,7 @@ const ServiceCard = ({
   image: string;
 }) => {
   return (
-    <div className="flex flex-col md:w-3/4 h-full justify-around items-center gap-4 p-4 bg-gradient-to-br from-primary to-secondary text-center rounded border border-2 border-white">
+    <div className="flex flex-col md:w-3/4 h-full justify-around items-center gap-4 p-4 text-center bg-secondary text-white">
       <h4 className="text-md font-medium">{title}</h4>
       <div>
         <img src={image} alt="" className="w-full h-full" />
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -6,14 +6,33 @@ import Button from "./Button";
 
 const Services = () => {
   return (
-    <div className="flex flex-col justify-center items-center py-5 bg-black text-white">
+    <div className="flex flex-col justify-center items-center py-5 bg-white text-black">
       <h3 className="text-[2rem]">Services</h3>
-      <p className="text-[0.7rem] text-center mx-[3rem] lg:mx-[6rem]">
+      <p className="md:hidden text-[0.7rem] text-center mx-[3rem] lg:mx-[6rem]">
         Welcome to our Barbershop, where we merge classic styling with modern
         trends to redefine your look. Experience exceptional haircuts, detailed
         beard trimming, and premium grooming treatments from our highly skilled
         barbers. We promise a superior and comfortable grooming experience every
         time you visit.
+      </p>
+      <p className="hidden md:block text-[0.7rem] text-center mx-[3rem] lg:mx-[6rem]">
+        Step into our Barbershop and immerse yourself in a unique blend of
+        tradition and contemporary style. We're not just a barbershop; we're a
+        destination for those seeking an unparalleled grooming experience. Our
+        professional team of barbers, with their rich expertise and an in-depth
+        understanding of styles and trends, are committed to providing you with
+        a unique and personalized service. From classic haircuts to the latest
+        styles, meticulous beard shaping to full grooming treatments, we cater
+        to all your grooming needs. We use top-tier products and modern tools to
+        ensure the highest quality service.
+        <br />
+        <br />
+        Our attention to detail and dedication to individual client needs set us
+        apart in our field. Here, every appointment is more than just a haircut;
+        it's an experience, an art, and above all, a representation of your
+        personal style. Discover the range of our services and let us craft the
+        perfect look that reflects your personality and lifestyle. Your journey
+        towards a great style begins here at our Barbershop!
       </p>
       <hr className="w-5/6 my-5" />
       <div className="grid grid-cols-2 md:grid-cols-3 md:place-items-center gap-[1rem] w-full px-4">
@@ -32,11 +51,13 @@ const Services = () => {
           text="Indulge in our comprehensive grooming package for a complete style transformation."
           image={barber}
         />
-        <div className="md:col-span-3 place-self-center text-center">
+        <div className="md:col-span-3 place-self-center text-center w-3/4">
           <p className="text-md mb-4">
-            See all our services and what we offer below
+            View all our services and what we offer below
           </p>
-          <Button text="See All" />
+          <div className="text-white">
+            <Button text="See All Services" />
+          </div>
         </div>
       </div>
     </div>
