822bd7ed917b66d5aba6dba53d7e494f7a4951d9
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-05 02:23:48 -0600

    Add images, reviews and footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Products from "./components/Products";
+import haircutImage from "./assets/testing.jpg";
+import haircutImage2 from "./assets/Haircut.png";
+import Reviews from "./components/Reviews";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +15,12 @@ function App() {
       <Hero />
       <Services />
       <Products />
+      <div className="hidden md:flex h-[50vh] w-full">
+        <img src={haircutImage} alt="" className="w-1/2" />
+        <img src={haircutImage2} alt="" className="w-1/2" />
+      </div>
+      <Reviews />
+      <Footer />
     </Layout>
   );
 }
