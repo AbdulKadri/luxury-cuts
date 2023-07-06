4aedad12c58f937b7efad5d1fe9145c1ba052826
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-06 16:49:35 -0600

    Change hover styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 const Button = ({ text = "Book Now" }: { text?: string }) => {
   return (
     <button
-      className={`py-1 px-2 border border-white bg-primary hover:bg-primary hover:text-white hover:border-white`}
+      className={`py-1 px-2 border border-white bg-primary hover:bg-green-700  font-semibold`}
     >
       {text}
     </button>
