4131ced4c6ef66fc0b555a156bed0404c9142da6
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-06 23:24:14 -0600

    Add Header, Footer and Scroll to top components

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,20 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ScrollToTop from "./utils/ScrollToTop";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <div className="font-poppins">{children}</div>;
+  return (
+    <div className="font-poppins">
+      <ScrollToTop />
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
