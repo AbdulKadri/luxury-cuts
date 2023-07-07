72dfe6ef80cc756da20e9f0a07b9509c4a4d6539
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-06 19:36:06 -0600

    Add all pages to router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.tsx";
 import "./index.css";
+import App from "./App.tsx";
+import ServicesPage from "./pages/ServicesPage.tsx";
+import ProductsPage from "./pages/ProductsPage.tsx";
+import ContactPage from "./pages/ContactPage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/services",
+    element: <ServicesPage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
   },
 ]);
 
