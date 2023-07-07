9cf9f1acb5d128ac085941a9a36e7244c6634471
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-06 19:37:20 -0600

    Change styles for  desktop and mobile

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -3,7 +3,7 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const ReviewCard = () => {
   return (
-    <div className="relative bg-gray-500 my-5 p-5 rounded w-3/4 md:w-[23rem]">
+    <div className="relative bg-gray-500 m-3 p-3 rounded w-auto">
       <h4 className="font-bold">Jack Crown</h4>
       <p className="text-gray-100 text-xs">2022-04-11</p>
       <div className="absolute top-5 right-5 w-[2.5rem] md:w-[3rem]">
@@ -14,7 +14,7 @@ const ReviewCard = () => {
       <FontAwesomeIcon icon={faStar} color="gold" />
       <FontAwesomeIcon icon={faStar} color="gold" />
       <FontAwesomeIcon icon={faStar} color="gold" />
-      <p className="mt-4 text-sm w-full md:w-[15rem] lg:w-[20rem]">
+      <p className="mt-4 text-sm w-full">
         Satisfaction guaranteed! Honestly the best work I have had in the YEG
         area. It’s hard to find someone who can do beards in the first place.
         Let alone someone who can shape it so it looks good tell my next stop
